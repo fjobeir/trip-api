17cb13aacdea45f5185e2fdb08b5d8e431b692a8
Feras Jobeir <fjobeir@gmail.com>
2022-10-13 14:54:25 +0300

    Trip validation

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -8,22 +8,21 @@ var store = async (req, res, next) => {
         data: {}
     }
 
-    var title = req.body.title.trim()
-    var cost = req.body.cost.trim()
-    var date = req.body.date.trim()
+    var title = req?.body?.title?.trim()
+    var cost = Number(req?.body?.cost)
+    var date = req?.body?.date?.trim()
 
-
-    if (title.length < 3) {
+    if (!title || title?.length < 3) {
         response.succeess = false
-        response.massages.push('title is not true')
+        response.massages.push('The title length should be more than 2')
     }
-    if (cost.length < 0) {
+    if (!cost || cost < 1) {
         response.succeess = false
         response.massages.push('please enter a valid number')
     }
-    if (date.length < 10) {
+    if (!(/^(((\d{4})(-)(0[13578]|10|12)(-)(0[1-9]|[12][0-9]|3[01]))|((\d{4})(-)(0[469]|11)(-)([0][1-9]|[12][0-9]|30))|((\d{4})(-)(02)(-)(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)(-)(02)(-)(29))|(([13579][26]00)(-)(02)(-)(29))|(([0-9][0-9][0][48])(-)(02)(-)(29))|(([0-9][0-9][2468][048])(-)(02)(-)(29))|(([0-9][0-9][13579][26])(-)(02)(-)(29)))(\s([0-1][0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))$/.test(date))) {
         response.succeess = false,
-            response.massages.push('please check your date')
+        response.massages.push('please check your date')
     }
     if (!response.succeess) {
         res.send(response)
