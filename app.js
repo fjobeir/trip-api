2274de4ca08994d34d19f7be362d52aacd6a4b2f
Feras Jobeir <fjobeir@gmail.com>
2022-10-19 10:47:40 +0300

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var membersRouter = require('./routes/members');
@@ -12,7 +13,7 @@ var subscriptionsRouter = require('./routes/subscriptions');
 var adminsRouter = require('./routes/admins');
 
 var app = express();
-
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
